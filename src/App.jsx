467ca2afab632daa9877816f0894c1014d548ab5
lovro <lovro.zagar5@gmail.com>
2023-04-04 12:20:29 -0900

    style: add mui theme to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Box, createTheme, ThemeProvider } from '@mui/material'
+import CV from './Components/CV'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#000000',
+      mainHover: '#222222',
+      text: '#ffffff',
+    },
+    secondary: {
+      main: '#999999',
+    },
+  },
+})
+
 function App() {
-  return <div className='App'></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ p: '1rem' }}>
+        <CV />
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
