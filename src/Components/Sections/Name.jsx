e074f244aba38d0a6a09c4f4a074b5303426f225
lovro <lovro.zagar5@gmail.com>
2023-04-11 16:00:43 -0900

    fix: make forename && surname mandatory, show placeholder value if no value

--- src/Components/Sections/Name.jsx
+++ src/Components/Sections/Name.jsx
@@ -1,14 +1,44 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { Box, TextField, Typography } from '@mui/material'
 import HoverContainer from '../Containers/HoverContainer'
 
 function Name() {
-  const namePlaceholder = {
-    forename: 'Forename',
-    surname: 'Surname',
-  }
+  const namePlaceholder = useMemo(() => {
+    return {
+      forename: 'Forename',
+      surname: 'Surname',
+    }
+  }, [])
+
   const [onEdit, setOnEdit] = useState(false)
   const [name, setName] = useState(namePlaceholder)
+
+  useEffect(() => {
+    if (name.forename.includes(' ') && !onEdit) {
+      setName({ ...name, forename: name.forename.trim() })
+    }
+    if (name.surname.includes(' ') && !onEdit) {
+      setName({ ...name, surname: name.surname.trim() })
+    }
+  }, [name, onEdit])
+
+  useEffect(() => {
+    if (!name.forename && !onEdit) {
+      setName({ ...name, forename: 'Forename' })
+    }
+    if (!name.surname && !onEdit) {
+      setName({ ...name, surname: 'Surname' })
+    }
+  }, [name, namePlaceholder, onEdit])
+
+  useEffect(() => {
+    if (onEdit && name.forename === 'Forename') {
+      setName({ ...name, forename: '' })
+    }
+    if (onEdit && name.surname === 'Surname') {
+      setName({ ...name, surname: '' })
+    }
+  }, [name, onEdit])
 
   function handleOnChange(e, key) {
     setName({ ...name, [key]: e.target.value })
@@ -25,14 +55,14 @@ function Name() {
         }}
       >
         {onEdit
-          ? Object.keys(name).map((key) => {
+          ? Object.keys(name).map((key, index) => {
               return (
                 <TextField
                   key={key}
                   value={name[key]}
                   inputProps={{ maxLength: 12 }}
                   onChange={(e) => handleOnChange(e, key)}
-                  placeholder={key === 'Forename' ? 'Forename' : 'Surname'}
+                  placeholder={index === 0 ? 'Forename' : 'Surname'}
                   sx={{
                     maxLength: '10',
                     flex: '1',
