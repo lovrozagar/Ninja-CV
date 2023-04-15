5aac1486669b283c493c146363be9f2b93c0c557
lovro <lovro.zagar5@gmail.com>
2023-04-15 12:24:24 -0900

    fix: change CV to not override the id's of section components on componentAdd to prevent re-render

--- src/Components/CV.jsx
+++ src/Components/CV.jsx
@@ -1,23 +1,55 @@
 import { Paper } from '@mui/material'
 import Name from './Sections/Name'
-
-import PrimaryButton from './Buttons/PrimaryButton'
-import SecondaryButton from './Buttons/SecondaryButton'
+import Position from './Sections/Position'
+import Links from './Sections/Links'
+import AddSection from './Dropdowns/AddSection'
+import Skills from './Sections/Skills'
+import WorkExperience from './Sections/WorkExperience'
+import uniqid from 'uniqid'
+import { useState } from 'react'
 
 function CV() {
+  // HOOKS
+  const [sections, setSections] = useState([])
+
+  // HANDLERS
+  function handleAdd(e) {
+    setSections((prev) => [...prev, { name: e.target.value, id: uniqid() }])
+  }
+
+  //HELPERS
+  function getSectionType(section) {
+    let component = null
+
+    switch (section) {
+      case 'Skills':
+        component = <Skills key={section.id} />
+        break
+      case 'Work Experience':
+        component = <WorkExperience key={section.id} />
+        break
+      default:
+        break
+    }
+
+    return component
+  }
+
   return (
     <Paper
       sx={{
         maxWidth: '210mm',
         minHeight: '295mm',
         m: '0 auto',
-        p: '0.5rem',
+        p: '0 0.5rem 0.5rem 0.5rem',
         textAlign: 'center',
       }}
     >
       <Name />
-      <PrimaryButton>Preview</PrimaryButton>
-      <SecondaryButton>Download</SecondaryButton>
+      <Position />
+      <Links />
+      {sections.map((section) => getSectionType(section.name))}
+      <AddSection onAdd={handleAdd} />
     </Paper>
   )
 }
