6e22a7b2489303549e7a419e98b70c1933816f46
lovro <lovro.zagar5@gmail.com>
2023-04-19 08:58:04 -0900

    refactor Points component so that it can be dynamically used as an inline or block list

--- src/Components/Text/Points.jsx
+++ src/Components/Text/Points.jsx
@@ -1,19 +1,53 @@
 import { Box, Typography } from '@mui/material'
 import Flex from '../Containers/Flex'
 
-function Points({ array }) {
+function Points({ array, display }) {
+  const bullet = <>&#183;</>
+
   return (
-    <Box>
-      {array.map((point, index) => (
-        <Flex key={index}>
-          <Typography lineHeight={1} fontSize={18} fontWeight='bold'>
-            &#183;
-          </Typography>
-          <Typography fontSize={13} fontWeight='400'>
-            {point.text}
-          </Typography>
-        </Flex>
-      ))}
+    <Box
+      display={(display || null) !== 'inline' ? 'block' : 'flex'}
+      justifyContent='center'
+      flexWrap='wrap'
+      gap={1}
+    >
+      {array.map((point, index) => {
+        return (
+          <>
+            {point.text.trim() !== '' && (
+              <Flex
+                key={index}
+                sx={{
+                  width: 'auto',
+                  flexWrap: 'nowrap',
+                  textAlign: 'start',
+                  textJustify: 'inter-word',
+                }}
+              >
+                <Typography
+                  display='inline'
+                  textAlign='start'
+                  lineHeight={1}
+                  fontSize={18}
+                  fontWeight='bold'
+                  sx={{ textAlignLast: 'left' }}
+                >
+                  {bullet}
+                </Typography>
+                <Typography
+                  display='inline'
+                  textAlign='start'
+                  fontSize={13}
+                  fontWeight='400'
+                  sx={{ textAlignLast: 'left', wordBreak: 'normal' }}
+                >
+                  {point.text}
+                </Typography>
+              </Flex>
+            )}
+          </>
+        )
+      })}
     </Box>
   )
 }
