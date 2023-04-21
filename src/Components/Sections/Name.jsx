6d77b7e9d32655f2772e0c107523e8b6c3d2bddd
lovro <lovro.zagar5@gmail.com>
2023-04-20 15:01:52 -0900

    refactor: rewrite NameView and NameEdit functionality in separate components using new reusable components

--- src/Components/Sections/Name.jsx
+++ src/Components/Sections/Name.jsx
@@ -1,55 +1,54 @@
 import { useState, useEffect, useMemo } from 'react'
 import { Box, TextField, Typography } from '@mui/material'
 import HoverContainer from '../Containers/HoverContainer'
+import Flex from '../Containers/Flex'
+import Grid from '../Containers/Grid'
+import SkewTitle from '../Titles/SkewTitle'
+import InputBlock from '../Inputs/InputBlock'
+import DynamicButton from '../Buttons/DynamicButton'
+import uniqid from 'uniqid'
 
 function Name() {
   const defaultPlaceholder = useMemo(() => {
     return {
-      forename: 'Forename',
-      surname: 'Surname',
+      forename: '',
+      surname: '',
     }
   }, [])
+  const names = useMemo(
+    () => [
+      { forename: 'John', surname: 'Doe' },
+      { forename: 'Jane', surname: 'Doe' },
+      { forename: 'Bob', surname: 'Smith' },
+      { forename: 'Alice', surname: 'Johnson' },
+      { forename: 'Mike', surname: 'Davis' },
+      { forename: 'Samantha', surname: 'Lee' },
+      { forename: 'Chris', surname: 'Wilson' },
+      { forename: 'Emily', surname: 'Garcia' },
+      { forename: 'David', surname: 'Hernandez' },
+      { forename: 'Jessica', surname: 'Rodriguez' },
+    ],
+    []
+  )
+
   const [onEdit, setOnEdit] = useState(false)
   const [name, setName] = useState(defaultPlaceholder)
+  const [placeholder, setPlaceholder] = useState([
+    { forename: 'John', surname: 'Doe' },
+  ])
 
   useEffect(() => {
-    if (name.forename.includes(' ') && !onEdit) {
-      setName((prevName) => ({
-        ...prevName,
-        forename: prevName.forename.trim(),
-      }))
+    if (!onEdit && name.forename.trim() === '' && name.surname.trim() === '') {
+      setPlaceholder(names[Math.floor(Math.random() * names.length)])
     }
-    if (name.surname.includes(' ') && !onEdit) {
-      setName((prevName) => ({ ...prevName, surname: prevName.surname.trim() }))
-    }
-  }, [name, onEdit])
+  }, [onEdit, name, names])
 
-  useEffect(() => {
-    if (!name.forename && !onEdit) {
-      setName((prevName) => ({
-        ...prevName,
-        forename: defaultPlaceholder.forename,
-      }))
-    }
-    if (!name.surname && !onEdit) {
-      setName((prevName) => ({
-        ...prevName,
-        surname: defaultPlaceholder.surname,
-      }))
-    }
-  }, [name, defaultPlaceholder, onEdit])
-
-  useEffect(() => {
-    if (onEdit && name.forename === defaultPlaceholder.forename) {
-      setName((prevName) => ({ ...prevName, forename: '' }))
-    }
-    if (onEdit && name.surname === defaultPlaceholder.surname) {
-      setName((prevName) => ({ ...prevName, surname: '' }))
-    }
-  }, [name, onEdit, defaultPlaceholder])
-
-  function handleOnChange(e, key) {
+  function handleChange(e, key) {
     setName((prevName) => ({ ...prevName, [key]: e.target.value }))
+  }
+
+  function handleDone() {
+    setOnEdit(false)
   }
 
   return (
@@ -63,42 +62,74 @@ function Name() {
           minHeight: '3.5rem',
         }}
       >
-        {Object.keys(name).map((key, index) => {
-          const isForename = key === 'forename'
-          return onEdit ? (
-            <TextField
-              key={key}
-              value={name[key] || ''}
-              inputProps={{ maxLength: 13 }}
-              onChange={(e) => handleOnChange(e, key)}
-              placeholder={
-                isForename
-                  ? defaultPlaceholder.forename
-                  : defaultPlaceholder.surname
-              }
-              sx={{
-                flex: '1',
-                '& input': {
-                  fontSize: '1rem',
-                },
-              }}
-            />
-          ) : (
-            <Typography
-              key={key}
-              variant='h3'
-              sx={{
-                textTransform: 'uppercase',
-                fontWeight: isForename ? '200' : '400',
-                textAlign: isForename ? 'right' : 'left',
-              }}
-            >
-              {name[key]}
-            </Typography>
-          )
-        })}
+        {onEdit ? (
+          <NameEdit
+            name={name}
+            forenamePlaceholder={placeholder.forename}
+            surnamePlaceholder={placeholder.surname}
+            onChange={handleChange}
+            onDone={handleDone}
+          />
+        ) : (
+          <NameView name={name} />
+        )}
       </Box>
     </HoverContainer>
+  )
+}
+
+function NameEdit({
+  name,
+  forenamePlaceholder,
+  surnamePlaceholder,
+  onChange,
+  onDone,
+}) {
+  return (
+    <Grid gap={1.5}>
+      <SkewTitle title='Name' color='primary.opposite' bgcolor='primary.main' />
+      <Grid type='1fr 1fr'>
+        <InputBlock
+          htmlFor={uniqid()}
+          name='Forename'
+          value={name.forename}
+          color='primary.opposite'
+          bgcolor='primary.violet'
+          placeholder={`e.g. ${forenamePlaceholder}`}
+          onChange={(e) => onChange(e, 'forename')}
+        />
+        <InputBlock
+          htmlFor={uniqid()}
+          name='Surname'
+          value={name.surname}
+          color='primary.opposite'
+          bgcolor='primary.violet'
+          placeholder={`e.g. ${surnamePlaceholder}`}
+          onChange={(e) => onChange(e, 'surname')}
+        />
+      </Grid>
+      <Flex type='end'>
+        <DynamicButton
+          text='Done'
+          type='button done contained medium'
+          mainColor='black'
+          onClick={onDone}
+        />
+      </Flex>
+    </Grid>
+  )
+}
+
+function NameView({ name }) {
+  return (
+    <Flex gap={1.5} type='center'>
+      <Typography variant='h4' sx={{ fontSize: '2.5rem', fontWeight: '200' }}>
+        {name.forename.trim() || 'Forename'}
+      </Typography>
+      <Typography variant='h4' sx={{ fontSize: '2.5rem', fontWeight: '500' }}>
+        {name.surname.trim() || 'Surname'}
+      </Typography>
+    </Flex>
   )
 }
 
