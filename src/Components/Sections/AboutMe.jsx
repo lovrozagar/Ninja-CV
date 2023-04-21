e78caab162757ec259ef1e517485399400cb7ecf
lovro <lovro.zagar5@gmail.com>
2023-04-21 08:49:48 -0900

    feat: add edit functionality for the AboutMe section

---
+++ src/Components/Sections/AboutMe.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import HoverContainer from '../Containers/HoverContainer'
+import Grid from '../Containers/Grid'
+import InputBlock from '../Inputs/InputBlock'
+import InputAreaBlock from '../Inputs/InputAreaBlock'
+import uniqid from 'uniqid'
+
+function AboutMe({ onDelete }) {
+  const defaultTitle = 'About Me'
+  const defaultAboutMe = [
+    {
+      text: 'As a web developer, I am passionate about creating engaging and user-friendly websites that meet the needs of businesses and their customers.',
+      id: uniqid(),
+    },
+    {
+      text: 'With a strong background in web development, I am proficient in coding, testing, and deploying websites across multiple platforms.',
+      id: uniqid(),
+    },
+  ]
+  const [onEdit, setOnEdit] = useState(false)
+  const [title, setTitle] = useState(defaultTitle)
+  const [aboutMe, setAboutMe] = useState(defaultAboutMe)
+
+  function handleTitleReset() {
+    setTitle(defaultTitle)
+  }
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value)
+  }
+
+  function handleParagraphChange(e, id) {
+    setAboutMe((prev) =>
+      prev.map((paragraph) => {
+        if (paragraph.id !== id) return paragraph
+
+        return { ...paragraph, text: e.target.value }
+      })
+    )
+  }
+
+  return (
+    <HoverContainer onEdit={onEdit} fn={setOnEdit} onDelete={onDelete}>
+      <Grid>
+        {onEdit ? (
+          <AboutMeEdit
+            title={title}
+            aboutMe={aboutMe}
+            onTitleReset={handleTitleReset}
+            onTitleChange={handleTitleChange}
+            onParagraphChange={handleParagraphChange}
+          />
+        ) : (
+          <div></div>
+        )}
+      </Grid>
+    </HoverContainer>
+  )
+}
+
+function AboutMeEdit({
+  title,
+  aboutMe,
+  onTitleReset,
+  onTitleChange,
+  onParagraphChange,
+}) {
+  return (
+    <Grid>
+      <InputBlock
+        name='Section Title'
+        button='restore'
+        value={title}
+        color='primary.opposite'
+        bgcolor='primary.main'
+        onChange={onTitleChange}
+        onClick={onTitleReset}
+      />
+      {aboutMe.map((paragraph, index) => {
+        return (
+          <InputAreaBlock
+            key={index}
+            color='primary.opposite'
+            bgcolor='primary.violet'
+            name={`${index + 1}. Paragraph`}
+            value={paragraph.text}
+            onChange={(e) => onParagraphChange(e, paragraph.id)}
+          />
+        )
+      })}
+    </Grid>
+  )
+}
+
+export default AboutMe
