cb5dc244249c15e264db986b6c3538a46873dbd1
lovro <lovro.zagar5@gmail.com>
2023-04-27 02:05:52 -0900

    refactor: change the event listeners, track position of interaction start and end, conditionally trigger clickOutside, holdOutside and Snackbar

--- src/Components/Containers/ClickOutsideGuard.jsx
+++ src/Components/Containers/ClickOutsideGuard.jsx
@@ -1,51 +1,89 @@
 import { Box } from '@mui/material'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-function ClickOutsideGuard({ children, onClickOutside, onOutsideHold }) {
+function ClickOutsideGuard({
+  onEdit,
+  children,
+  onClickOutside,
+  onOutsideHold,
+  onSnackbarChange,
+}) {
+  const [onDown, setOnDown] = useState(null)
+  const [onDownIsWithin, setOnDownIsWithin] = useState(null)
   const wrapperRef = useRef(null)
 
   useEffect(() => {
-    function handleClickOutside(e) {
-      const clickedInsideSelectMenu = Boolean(e.target.closest('.MuiList-root'))
-      const clickedOnSelectMenu = Boolean(
-        e.target.classList.contains('MuiBackdrop-root')
-      )
-
-      if (
-        wrapperRef.current &&
-        !wrapperRef.current.contains(e.target) &&
-        !clickedOnSelectMenu &&
-        !clickedInsideSelectMenu
-      ) {
-        onClickOutside()
-        document.activeElement.blur()
-      }
-    }
-
     function handleHoldOutside(e) {
       if (wrapperRef.current && !wrapperRef.current.contains(e.target))
         onOutsideHold()
     }
 
-    document.addEventListener('mousedown', handleClickOutside)
-    document.addEventListener('touchstart', handleClickOutside)
+    const handleMouseDown = (e) => {
+      setOnDown(e.target)
+      setOnDownIsWithin(wrapperRef.current.contains(e.target))
+    }
+
+    const handleMouseUp = (e) => {
+      console.log(onDown, e.target)
+      if (!onEdit) return
+
+      const downOnPaper = Boolean(onDown.closest('.MuiPaper-root'))
+      const upOnPaper = Boolean(e.target.closest('.MuiPaper-root'))
+
+      if ((!downOnPaper && !upOnPaper) || !onDownIsWithin) onSnackbarChange()
+      if (!downOnPaper || !onDownIsWithin) {
+        onClickOutside()
+        document.activeElement.blur()
+      }
+    }
+
+    const handleTouchEnd = (e) => {
+      if (!onEdit) return
+
+      const downOnPaper = Boolean(onDown.closest('.MuiPaper-root'))
+      const upOnPaper = Boolean(
+        e.changedTouches[0].target.closest('.MuiPaper-root')
+      )
+
+      if (!downOnPaper && !upOnPaper) onSnackbarChange()
+      if (!downOnPaper || !wrapperRef.current.contains(e.target)) {
+        onClickOutside()
+        document.activeElement.blur()
+      }
+    }
+
+    document.addEventListener('mousedown', handleMouseDown)
+    document.addEventListener('touchstart', handleMouseDown)
+    document.addEventListener('mouseup', handleMouseUp)
+    document.addEventListener('touchend', handleTouchEnd)
     document.addEventListener('mousemove', handleHoldOutside)
+    document.addEventListener('touchmove', handleHoldOutside)
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-      document.removeEventListener('touchstart', handleClickOutside)
-      document.addEventListener('mousemove', handleHoldOutside)
+      document.removeEventListener('mousedown', handleMouseDown)
+      document.removeEventListener('touchstart', handleMouseDown)
+      document.removeEventListener('mouseup', handleMouseUp)
+      document.removeEventListener('touchend', handleTouchEnd)
+      document.removeEventListener('mousemove', handleHoldOutside)
+      document.removeEventListener('touchmove', handleHoldOutside)
     }
-  }, [onClickOutside, onOutsideHold])
+  }, [
+    onClickOutside,
+    onOutsideHold,
+    onSnackbarChange,
+    onEdit,
+    onDown,
+    onDownIsWithin,
+  ])
+
+  const guardStyling = {
+    bgcolor: 'primary.opposite',
+    webkitTouchCallout: 'none !important',
+    webkitUserSelect: 'none !important',
+  }
 
   return (
-    <Box
-      sx={{
-        webkitTouchCallout: 'none !important',
-        webkitUserSelect: 'none !important',
-      }}
-      ref={wrapperRef}
-    >
+    <Box sx={guardStyling} ref={wrapperRef}>
       {children}
     </Box>
   )
